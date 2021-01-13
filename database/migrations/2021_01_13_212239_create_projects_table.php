9eaf4bc74c5776181d65a6d4a1dc4799e2817369
Web Disciple <web.disciple.pro@gmail.com>
2021-01-13 22:49:43 +0100

    basic migration of project

--- database/migrations/2021_01_13_212239_create_projects_table.php
+++ database/migrations/2021_01_13_212239_create_projects_table.php
@@ -15,6 +15,8 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
