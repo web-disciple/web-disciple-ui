d126e8de9da2dcd0cfdceb35daff487f3bedf767
Web Disciple <web.disciple.pro@gmail.com>
2021-01-13 22:59:05 +0100

    project relationship with page

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    /**
+     * Get the pages for the project.
+     */
+    public function pages()
+    {
+        return $this->hasMany(Page::class);
+    }
 }
